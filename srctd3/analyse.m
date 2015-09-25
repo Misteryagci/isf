37a70638459474ab84f8a887c80f44dff45a3bf8
Misteryagci <kaanyagci@gmail.com>
2015-09-25 03:56:07 +0200

    Mise à jour de la fonciton analyse

--- srctd3/analyse.m
+++ srctd3/analyse.m
@@ -1,11 +1,13 @@
-function [s,mi,mj,sii,sij,sji,sjj,valeur_propres_mat_diag,mat_passage]=analyse(nf)
+function [i,s,mi,mj,sii,sij,sji,sjj,valeur_propres_mat_diag,mat_passage,ratio,theta]=analyse(nf)
   % nom_fichier : nom du fichier image au format pgm (en noir et blanc)
   [I,ngmax]=imload(nf);
+  i=I;
   printf ("La somme des valeurs de la matrice correspond à l'image %s est %d\n",nf,sum(sum(I)))
   s=sum(sum(I));
   [nbligne,nbcol]=size(I);
   %calcul des premiers moments de la matrice I
   mi = 0;
+  %parcours la matrice element par element
   for i = 1:nbligne
     for j = 1:nbcol
       mi=mi+I(i,j)*i;
@@ -13,6 +15,7 @@ function [s,mi,mj,sii,sij,sji,sjj,valeur_propres_mat_diag,mat_passage]=analyse(n
   endfor
   mi=mi/s; %C'est le moment de la matrice par rapport à i
   mj = 0;
+  %parcours la matrice element par element
   for i = 1:nbligne
     for j = 1:nbcol
       mj=mj+I(i,j)*j;
@@ -22,6 +25,7 @@ function [s,mi,mj,sii,sij,sji,sjj,valeur_propres_mat_diag,mat_passage]=analyse(n
    
   %Calcul des deuxièmes moments centrés de la matrice M
   sii = 0;
+  %parcours la matrice element par element
   for i = 1:nbligne
     for j = 1:nbcol
       sii=sii+I(i,j)*((i-mi)^2);
@@ -30,6 +34,7 @@ function [s,mi,mj,sii,sij,sji,sjj,valeur_propres_mat_diag,mat_passage]=analyse(n
   sii=sii/s; %C'est le moment centré de la matrice par rapport à ii
   
   sij = 0;
+  %parcours la matrice element par element
   for i = 1:nbligne
     for j = 1:nbcol
       sij=sij+I(i,j)*((i-mi)*(j-mj));
@@ -40,6 +45,7 @@ function [s,mi,mj,sii,sij,sji,sjj,valeur_propres_mat_diag,mat_passage]=analyse(n
   sji=sij;  
    
   sjj = 0;
+  %parcours la matrice element par element
   for i = 1:nbligne
     for j = 1:nbcol
       sii=sii+I(i,j)*((j-mj)^2);
@@ -60,6 +66,10 @@ function [s,mi,mj,sii,sij,sji,sjj,valeur_propres_mat_diag,mat_passage]=analyse(n
   [V,l] = eig(C);
   valeur_propres_mat_diag = l;
   mat_passage = V
+  ratio = sqrt(max(max(valeur_propres_mat_diag)) - min(min(valeur_propres_mat_diag)));
+  y=mat_passage(1,1);
+  x=mat_passage(2,1);
+  theta = atan2(y,x);
  endfunction
 
 
